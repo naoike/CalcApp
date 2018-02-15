633bf58cd45df2c8ecfdaa31165990e5b7f70336
naoike <ripokazu0627nao@yahoo.co.jp>
2018-02-15 15:38:44 +0900

    最初のcommit

---
+++ app/src/main/java/jp/techacademy/naoki/ikegami/calcapp/MainActivity.java
@@ -0,0 +1,82 @@
+package jp.techacademy.naoki.ikegami.calcapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    EditText mEditText1;
+    EditText mEditText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditText1 = (EditText) findViewById(R.id.editText1);
+        mEditText2 = (EditText) findViewById(R.id.editText2);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+
+        if (v.getId() == R.id.button1){
+            String str1 = mEditText1.getText().toString();
+            String str2 = mEditText2.getText().toString();
+            double i1 =  Double.parseDouble(str1);
+            double i2 =  Double.parseDouble(str2);
+
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra("VALUE",i1+i2);
+            startActivity(intent);
+
+        } else  if (v.getId() == R.id.button2){
+            String str1 = mEditText1.getText().toString();
+            String str2 = mEditText2.getText().toString();
+            double i1 =  Double.parseDouble(str1);
+            double i2 =  Double.parseDouble(str2);
+
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra("VALUE",i1-i2);
+            startActivity(intent);
+
+        } else  if (v.getId() == R.id.button3){
+            String str1 = mEditText1.getText().toString();
+            String str2 = mEditText2.getText().toString();
+            double i1 =  Double.parseDouble(str1);
+            double i2 =  Double.parseDouble(str2);
+
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra("VALUE",i1*i2);
+            startActivity(intent);
+
+        } else  if (v.getId() == R.id.button4){
+            String str1 = mEditText1.getText().toString();
+            String str2 = mEditText2.getText().toString();
+            double i1 =  Double.parseDouble(str1);
+            double i2 =  Double.parseDouble(str2);
+
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra("VALUE",i1/i2);
+            startActivity(intent);
+    }
+    }
+}
